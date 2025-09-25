64f416f3f9578d7ab2da33e73f4704c38d40706e
JakubHrica <kubo.hrica@gmail.com>
2025-09-25 13:07:35 +0200

    version_7

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-// Nastavenie odpočtu na 60 sekúnd od terajšieho času
-const countdownDate = new Date().getTime() + 60 * 1000; 
+// Nastavenie CIEĽOVÉHO DATUMU a ČASU pre odpočet
+// Tento dátum bude rovnaký pre všetky zariadenia
+// Príklad: 25. december 2025 o 10:00:00
+const countdownDate = new Date("Dec 25, 2025 13:10:00").getTime(); 
 
 const countdownDiv = document.getElementById('countdown');
 const countdownContainer = document.getElementById('countdown-container');
@@ -13,12 +15,14 @@ const updateCountdown = setInterval(() => {
     // Rozdiel medzi cieľovým a aktuálnym časom
     const distance = countdownDate - now;
     
-    // Výpočty pre minúty a sekundy
+    // Výpočty pre dni, hodiny, minúty a sekundy
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     // Zobrazenie výsledku
-    countdownDiv.innerHTML = `${minutes}m ${seconds}s`;
+    countdownDiv.innerHTML = `${days}d ${hours}h ${minutes}m ${seconds}s`;
     
     // Ak je odpočet hotový, zobrazí GIF a konfety
     if (distance < 0) {
@@ -31,7 +35,6 @@ const updateCountdown = setInterval(() => {
         launchConfetti();
     }
 }, 1000);
-
 
 // Funkcia na spustenie konfiet s knižnicou Confetti.js
 function launchConfetti() {
